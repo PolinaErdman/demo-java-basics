51c2c4112dcbce853a65453cd6c0698b195f0177
palinaerdman <polinaerdman@gmail.com>
2024-10-20 20:26:29 +0300

    refactor class BasePage

--- src/main/java/by/itacademy/polinaerdman/pages/BasePage.java
+++ src/main/java/by/itacademy/polinaerdman/pages/BasePage.java
@@ -3,7 +3,7 @@ package by.itacademy.polinaerdman.pages;
 public class BasePage {
     protected String BASE_URL = "BASE URL";
 
-    public void open(String BASE_URL) {
+    public void open() {
         System.out.println("Пользователь открыл страницу " + BASE_URL);
     }
 }
