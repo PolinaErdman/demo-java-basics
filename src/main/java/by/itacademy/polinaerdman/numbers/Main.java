b3ea4417b4814d4211013b122eb3f4ad7c0f64be
palinaerdman <polinaerdman@gmail.com>
2024-10-20 15:28:24 +0300

    refactor class Main (add method numberToWord)

--- src/main/java/by/itacademy/polinaerdman/numbers/Main.java
+++ src/main/java/by/itacademy/polinaerdman/numbers/Main.java
@@ -2,9 +2,24 @@ package by.itacademy.polinaerdman.numbers;
 
 public class Main {
     public static void main(String[] args) {
-        int number = 1;
-        String numberAsAString = "";
+        int number = 0;
+        String numberAsAString = numberToWord(number);
 
         System.out.println(numberAsAString);
+    }
+    private static String numberToWord(int number) {
+        switch (number) {
+            case 0 : return "zero";
+            case 1 : return "one";
+            case 2 : return "two";
+            case 3 : return "three";
+            case 4 : return "four";
+            case 5 : return "five";
+            case 6 : return "six";
+            case 7 : return "seven";
+            case 8 : return "eight";
+            case 9 : return "nine";
+            default: return "Not a digit!";
+        }
     }
 }
