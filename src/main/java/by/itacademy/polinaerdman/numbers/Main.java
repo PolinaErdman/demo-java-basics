1f2023ab347049f9acc8f586ed99531cb02600d8
palinaerdman <polinaerdman@gmail.com>
2024-10-20 15:26:23 +0300

    add class Main

---
+++ src/main/java/by/itacademy/polinaerdman/numbers/Main.java
@@ -0,0 +1,10 @@
+package by.itacademy.polinaerdman.numbers;
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 1;
+        String numberAsAString = "";
+
+        System.out.println(numberAsAString);
+    }
+}
