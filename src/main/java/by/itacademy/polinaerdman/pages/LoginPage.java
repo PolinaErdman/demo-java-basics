73fc3766dc4e65a49b03adb7e55222f158fce7db
palinaerdman <polinaerdman@gmail.com>
2024-10-20 20:33:01 +0300

    add class LoginPage

---
+++ src/main/java/by/itacademy/polinaerdman/pages/LoginPage.java
@@ -0,0 +1,14 @@
+package by.itacademy.polinaerdman.pages;
+
+public class LoginPage extends BasePage {
+    protected String LOGIN_PAGE = BASE_URL + "/форма логина";
+    String title = "Форма логина";
+
+    public void open() {
+        System.out.println("Пользователь открыл страницу " + LOGIN_PAGE);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
