95b294bcb0c0df46c942f92839e7570bb6c977e9
palinaerdman <polinaerdman@gmail.com>
2024-10-21 19:15:41 +0300

    refactor class Main

--- src/main/java/by/itacademy/polinaerdman/numbers/Main.java
+++ src/main/java/by/itacademy/polinaerdman/numbers/Main.java
@@ -2,24 +2,9 @@ package by.itacademy.polinaerdman.numbers;
 
 public class Main {
     public static void main(String[] args) {
-        int number = 0;
-        String numberAsAString = numberToWord(number);
-
-        System.out.println(numberAsAString);
-    }
-    private static String numberToWord(int number) {
-        switch (number) {
-            case 0 : return "zero";
-            case 1 : return "one";
-            case 2 : return "two";
-            case 3 : return "three";
-            case 4 : return "four";
-            case 5 : return "five";
-            case 6 : return "six";
-            case 7 : return "seven";
-            case 8 : return "eight";
-            case 9 : return "nine";
-            default: return "Not a digit!";
+        for (int i = 0; i < 11; i++) {
+            String numberAsAString = TranslatorNumbersToWord.numberToWord(i);
+            System.out.println(numberAsAString);
         }
     }
 }
