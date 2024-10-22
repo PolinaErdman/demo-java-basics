f7690401f61384d66849a51664c5af51bb7f5603
palinaerdman <polinaerdman@gmail.com>
2024-10-22 21:29:55 +0300

    add test class SiteTest

---
+++ src/test/java/SiteTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+public class SiteTest {
+
+    @Test
+    public void testSite1() {
+
+    }
+
+    @Test
+    public void testSite2() {
+
+    }
+
+    @Test
+    public void testSite3() {
+
+    }
+}
